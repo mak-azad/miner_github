5c01f9bd874174051da7bf4263b9971999bbf936
mak-azad <akazad@umich.edu>
2024-02-20 21:44:28 +0000

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Spliting task to all nodes..."
-python3 task_parallelizer.py repository_lists/repo.csv ubuntu
+python3 task_parallelizer.py repository_lists/github_repositories_C++_12232023.csv ubuntu
 echo "Now running the analyzer script.."
 parallel-ssh -i -h sshhosts -t 0 'python3 miner_github/analyzer/repo_analyzer.py'
