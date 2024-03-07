8ae57034ad34f93a12a8f0fab7c5224a9b34f1b4
mak-azad <akazad@umich.edu>
2024-03-07 08:45:49 +0000

    added nohup

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 echo "Spliting task to all nodes..."
 python3 task_parallelizer.py repository_lists/github_repositories_C++_12232023.csv ubuntu
 echo "Now running the analyzer script.."
-parallel-ssh -i -h sshhosts -t 0 'python3 miner_github/analyzer/repo_analyzer.py'
+parallel-ssh -i -h sshhosts -t 0 'nohup python3 miner_github/analyzer/repo_analyzer.py'
