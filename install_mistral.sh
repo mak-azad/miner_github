944f15a40646925e4752ceb72fa4b24220490875
mak-azad <akazad@umich.edu>
2024-04-14 22:37:02 +0000

    another bugfix

--- install_mistral.sh
+++ install_mistral.sh
@@ -9,6 +9,7 @@ git clone https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
 cd  Mistral-7B-Instruct-v0.2/ 
 git lfs fetch 
 git lfs pull 
+cd ..
 echo "Testing mistral...."
 sleep 5
 /home/ubuntu/miniforge3/bin/python home/ubuntu/miner_github/test_mistral7b.py
