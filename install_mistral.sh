8b96fdd1c5f1b89fd512915e679557b7739a342e
mak-azad <akazad@umich.edu>
2024-04-14 22:30:07 +0000

    mistral bugfix

--- install_mistral.sh
+++ install_mistral.sh
@@ -2,11 +2,13 @@
 # Initialize Conda for script use
 # Using parallel-ssh to install Python packages on all hosts
 
-parallel-ssh -i -h sshhosts -t 0 'pip3 install langchain -y'
-parallel-ssh -i -h sshhosts -t 0 'pip3 install bitsandbytes -y'
-parallel-ssh -i -h sshhosts -t 0 'sudo apt install git-lfs'
-parallel-ssh -i -h sshhosts -t 0 'git clone https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2'
-parallel-ssh -i -h sshhosts -t 0 'cd  Mistral-7B-Instruct-v0.2/ && git lfs fetch -y && git lfs pull -y'
+pip3 install langchain -y
+pip3 install bitsandbytes -y
+sudo apt install git-lfs -y
+git clone https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2
+cd  Mistral-7B-Instruct-v0.2/ 
+git lfs fetch -y
+git lfs pull -y
 echo "Testing mistral...."
 sleep 5
 python home/ubuntu/miner_github/test_mistral7b.py
